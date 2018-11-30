5b1c526c4fec73b169bc572f6c4621040e44490b
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-11-30 12:15:50 +0900

    11/30 add tablefactory package

---
+++ abstractfactory/tablefactory/TableFactory.java
@@ -0,0 +1,22 @@
+package tablefactory;
+
+import factory.Factory;
+import factory.Link;
+import factory.Tray;
+import factory.Page;
+
+public class TableFactory extends Factory {
+    @Override
+    public Link createLink(String caption, String url) {
+        return new TableLink(caption, url);
+    }
+    @Override
+    public Tray createTray(String caption) {
+        return new TableTray(caption);
+    }
+    @Override 
+    public Page createPage(String title, String author) {
+        return new TablePage(title, author);
+    }
+
+}
