d4e5c68d591d9f89b1f72dfa3467d298810d32e6
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-12-31 06:54:24 +0900

    Memento

---
+++ 18_memento/game/Memento.java
@@ -0,0 +1,25 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Memento {
+    int money;
+    ArrayList<String> fruits;
+    public int getMoney() { // (narrow interface)
+        return money;
+    }
+    Memento(int money) { // (wide interface)
+        this.money = money;
+        this.fruits = new ArrayList<String>();
+    }
+    void addFruit(String fruit) { // (wide interface)
+        fruits.add(fruit);
+    }
+    List<String> getFruits() { // (wide interface)
+        //ArrayList<String> ret = new ArrayList<>(fruits);
+        return new ArrayList<>(fruits);
+    }
+
+}
+
